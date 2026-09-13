8457d159d8af970992a2a7a655ad127ac74cc752
Rishank Kumar Singh <onlyforsuccess1998@gmail.com>
2026-09-13 23:37:21 +0530

    Add English Grammar to Original Practice

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-// v39 Environment and Ecology Original Tricks 20260912
-const CACHE_VERSION = "efp-pwa-2026-09-12-v71-ecology-tricks";
+// v40 English Grammar Original Practice 20260913
+const CACHE_VERSION = "efp-pwa-2026-09-13-v72-english-practice";
 const OWNER_DEBUG_SCRIPT = '<script src="/owner-debug.js?v=20260911owner1"></script>';
 const OWNER_STATE_CACHE = "efp-owner-settings-v1";
 const OWNER_STATE_REQUEST = "/__efp_owner_debug_state__";
@@ -36,6 +36,7 @@ const APP_SHELL = [
   "/Original%20Practice/index.html",
   "/Original%20Practice/original-practice.css",
   "/Original%20Practice/original-practice.js?v=20260908answerreset2",
+  "/Original%20Practice/english-practice.js?v=20260913english1",
   "/Original%20Practice/original-practice-index.js",
   "/Crux-Tricks/index.html",
   "/Crux-Tricks/viewer.html",
