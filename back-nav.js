29e722857a763b7416edfc181599ec476ac81ef3
Rishank Kumar Singh <onlyforsuccess1998@gmail.com>
2026-09-24 22:02:29 +0530

    Fix website-only app link handling

--- back-nav.js
+++ back-nav.js
@@ -957,13 +957,3 @@ installCruxHomeSearchHistoryGuard();
 
 })();
 
-
-/* Site-wide transient Open-in-App banner loader. */
-(function () {
-  if (document.getElementById("efp-open-app-script")) return;
-  var script = document.createElement("script");
-  script.id = "efp-open-app-script";
-  script.src = "/open-in-app.js?v=20260924openapp1";
-  script.defer = true;
-  (document.head || document.documentElement).appendChild(script);
-})();
